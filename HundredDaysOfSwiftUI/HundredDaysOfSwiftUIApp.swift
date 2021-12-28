217a036732918fba8f7599824d844758bfc97084
Max <maxdgladkov@gmail.com>
2021-12-28 14:44:38 +0300

    Initial Commit

---
+++ HundredDaysOfSwiftUI/HundredDaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HundredDaysOfSwiftUIApp.swift
+//  HundredDaysOfSwiftUI
+//
+//  Created by Max Gladkov on 28.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct HundredDaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
